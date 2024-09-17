c9f3330f97dbe9adba67eba7c82e6927de09bd2d
M.Rahimi <mmadrhimi19@gmail.com>
2024-09-17 15:25:07 +0330

    install & add all library

---
+++ TypeScript Test/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TodoList from './components/TodoList.vue';
+</script>
+
+<template>
+  <header>
+   <TodoList />
+  </header>
+
+</template>
+
+<style>
+</style>
---
+++ TypeScript Test/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      screens:{
+        'sm': '340px',
+        
+        'md': '640px',
+
+        'lg': '768px',
+  
+        'xl': '1024px',
+  
+        '2xl': '1280px',
+  
+        '3xl': '1536px',
+      }
+    },
+  },
+  plugins: [],
+}
+
