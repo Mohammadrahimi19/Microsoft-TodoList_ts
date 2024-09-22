9b3a3230432f7de63684335364d56fe60c6920a5
M.Rahimi <mmadrhimi19@gmail.com>
2024-09-22 16:31:19 +0330

    I fixed the bugs and went my own way
    - I created three lists (AllTasks - incomplated - CompletedTask).
    
    Result: Proz was very challenging and instructive

--- TypeScript/src/components/MainTodo/MainTodo.vue
+++ TypeScript/src/components/MainTodo/MainTodo.vue
@@ -1,52 +1,52 @@
 <template>
   <div ref="warpper" class="warpper">
-      <div  v-for="(item, index) in CompletedTask" :key="index">
-        <div ref="uncompletedItems" v-if="!item.Iscompletd" class="Task uncompleted">
-            <div class="flex flex-row gap-2 items-center">
-              <input
-                type="checkbox"
-                :name="item.Name"
-                v-model="item.Iscompletd"
-                id=""
-                class="w-10 h-10 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
-              <input disabled="true" ref="name" type="text"  class="text-3xl border-none bg-transparent focus:border-2 border-blue-950 focus:shadow-2xl duration-300 rounded-md h-10 focus:bg-blue-700 focus:bg-opacity-90 focus:text-white"   :value="item.Name" >
-            </div>
-            <div class="flex flex-row gap-5 items-center">
-                  <button @click="item.Liked = !item.Liked">
-                  <FontAwesomeIcon
-                    
-                    :class="item.Liked ? 'text-yellow-400' : 'text-black'"
-                    :icon="item.Liked ? ['fas', 'fa-star'] : ['far', 'fa-star']"
-                  />
-                </button>
-                <button @click="changeEdit(index)" >
-                  <FontAwesomeIcon
-                  title="ویرایش"
-                    class=" text-blue-950/100 hover:animate-pulse"
-                    :icon=" ['fas','fa-pen']"
-                  />
-                </button>
-                <button  @click="DeleteItem(item.id!)" >
-                  {{ item.id }}
+      <div ref="ELements" :Elem="item.id" v-for="(item, index) in incomplated" :key="index">
+          <div  class="Task uncompleted">
+              <div class="flex flex-row gap-2 items-center">
+                <input
+                  type="checkbox"
+                  :name="item.Name"
+                  v-model="item.Iscompletd"
+                  id=""
+                  class="w-10 h-10 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <input disabled="true" ref="name" type="text"  class="text-3xl border-none bg-transparent focus:border-2 border-blue-950 focus:shadow-2xl duration-300 rounded-md h-10 focus:bg-blue-700 focus:bg-opacity-90 focus:text-white"   :value="item.Name" >
+              </div>
+              <div class="flex flex-row gap-5 items-center">
+                    <button @click="item.Liked = !item.Liked">
+                    <FontAwesomeIcon
+                      
+                      :class="item.Liked ? 'text-yellow-400' : 'text-black'"
+                      :icon="item.Liked ? ['fas', 'fa-star'] : ['far', 'fa-star']"
+                    />
+                  </button>
+                  <button @click="changeEdit(index)" >
+                    <FontAwesomeIcon
+                    title="ویرایش"
+                      class=" text-blue-950/100 hover:animate-pulse"
+                      :icon=" ['fas','fa-pen']"
+                    />
+                  </button>
+                  <button  @click="DeleteItem(item.id)" >
+                    {{ item.id }}
 
-                  <FontAwesomeIcon
-                  title="حذف"
-                    class="text-red-600 hover:animate-pulse"
-                    :icon=" ['fas','fa-trash-can']"
-                  />
-                </button>
-            </div>
-        </div>
+                    <FontAwesomeIcon
+                    title="حذف"
+                      class="text-red-600 hover:animate-pulse"
+                      :icon=" ['fas','fa-trash-can']"
+                    />
+                  </button>
+              </div>
+          </div>
       </div>
 
 
-      <p v-if="checkTask" class="text-2xl bg-black bg-opacity-80 text-white w-max p-5 rounded-lg">
+      <p v-if="ChackTitle" class="text-2xl bg-black bg-opacity-80 text-white w-max p-5 rounded-lg">
         complete
       </p>
 
-      <div v-for="(item, index) in CompletedTask" :key="index">
-        <div  v-if="item.Iscompletd" class="Task completed">
+      <div ref="ELements" :Elem="item.id" v-for="(item, index) in CompletedTask" :key="index">
+        <div   class="Task completed">
           <div class="flex flex-row gap-2 items-center">
             <input
               type="checkbox"
@@ -63,7 +63,7 @@
               :icon="item.Liked ? ['fas', 'fa-star'] : ['far', 'fa-star']"
             />
           </button>
-          <button  @click="DeleteItem(item.id!)" >
+          <button  @click="DeleteItem(item.id)" >
             {{ item.id }}
                   <FontAwesomeIcon
                   title="حذف"
@@ -77,70 +77,124 @@
 </template>
 
 <script  lang="ts" setup>
-import { computed, nextTick, ref, watchEffect } from 'vue'
+import {  nextTick, ref, watchEffect ,watch} from 'vue'
 import { Task } from '../../interFace/interface'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import gsap from 'gsap';
-const uncompletedItems =ref<HTMLDivElement[]>([])
 
+const ELements =ref<HTMLDivElement[]>([])
 const Props = defineProps<{ NewTask?: Task | null }>()
 
 const warpper = ref<HTMLDivElement | null>()
+const AllTasks = ref<Task[]>([])
 const CompletedTask = ref<Task[]>([])
+const incomplated =ref<Task[]>([])
+const  emit = defineEmits(['claer'])
 
-const checkTask = computed(() => {
-  let res=ref<boolean>(false)
 
-  res.value = CompletedTask.value.some(item=>{
-    if(item.Iscompletd){ return true }
-  })
-  return res.value
-})
 
 const name= ref<HTMLInputElement[]>([])
 const changeEdit =(index:number):void=>{
   name.value[index].disabled = !name.value[index].disabled
   name.value[index].focus()
-  document.addEventListener('keydown',(e:Event)=>{
+  document.addEventListener('keydown',(e:KeyboardEvent)=>{
       if(e.key === 'Enter'){
         name.value[index].disabled = !name.value[index].disabled
       }
   })
 }
 
-const DeleteItem =(id:number)=>{
-  const index = CompletedTask.value.findIndex(item=>item.id === id)  
-  UpdateElement(false,index)
+const DeleteItem =(id:number | undefined)=>{
+  
+  const index = AllTasks.value.findIndex(item=>item.id === id)  
+  UpdateElement(false,index,id)
 }
 
-let  emit = defineEmits(['claer'])
+
+
+
+watch(AllTasks,()=>{
+    AllTasks.value.forEach((item)=>{
+      
+     if(!item.Iscompletd &&  !incomplated.value.some(item2=>item2 === item )){
+        incomplated.value.push(item)
+        CompletedTask.value.forEach((item,index)=>{
+            if(!item.Iscompletd){
+              CompletedTask.value.splice(index,1)
+            }
+        })
+      
+        
+     }else if(item.Iscompletd &&  !CompletedTask.value.some(item2=>item2 === item )) {
+      incomplated.value.forEach((item,index)=>{
+            if(item.Iscompletd){
+              incomplated.value.splice(index,1)
+            }
+        })
+      CompletedTask.value.push(item)
+     }
+    })
+    
+
+    CheckTitleFunc()
+    
+ 
+},{deep:true})
+const ChackTitle = ref<boolean>(false)
 watchEffect( async() => {
   
   if (Props.NewTask) {
     const NewObj: Task = { ...Props.NewTask }
-    NewObj.id = CompletedTask.value.length + 1
-    CompletedTask.value.push(NewObj)
+    NewObj.id = AllTasks.value.length + 1
+    AllTasks.value.push(NewObj)
 
     await nextTick()
     UpdateElement(true)
+    
+  
+
     emit('claer')
   }
 })
 
+const CheckTitleFunc = ():void =>{
+      if(AllTasks.value.some(item=>item.Iscompletd)){
+        ChackTitle.value = true
+      }else{
+        ChackTitle.value = false
+      }
+}
 
 
 
 // اعمال انیمیشن  برای اد و حذف کردن تودو
-const UpdateElement=(status:boolean,index?:number):void =>{
+const UpdateElement=(status:boolean,index?:number,id?:number):void =>{
     if(status){
-      uncompletedItems.value.forEach((item  , index) => {
-        if(index === CompletedTask.value.length -1){
-          gsap.from(item, { opacity: 0, y: 50, duration: 0.5, ease: 'back.out(1.7)' });
+      console.log(AllTasks.value);
+      
+      ELements.value.forEach((item  , index) => {
+        if(index === AllTasks.value.length -1){
+          gsap.from(item, { opacity: 0, y: 50, duration: 0.5, ease: 'back.out(1.7)'});
         }
       });
     }else {
-      CompletedTask.value.splice(index!,1)
+      ELements.value.forEach((item)=>{
+        if(Number(item.getAttribute('Elem')) === id){
+          console.log(item);
+          gsap.to(item,{
+          opacity: 0, y: 200, duration: 0.5, ease: 'back.out(1.7)',display:'none',onComplete:()=>{
+            CheckTitleFunc()
+            AllTasks.value.splice(index!,1)
+            console.log(AllTasks.value);
+          }
+        })        
+        }  
+        })
+      
+   
     }
+    
+
 }
 
 
